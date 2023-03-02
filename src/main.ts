d6e21eedb571b2043ffb536e60490d68f10ccd90
Rehman Ahmed Khan <rehmankha73@gmail.com>
2023-03-02 09:38:00 +0500

    small update

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const port = 6000;
+  const app = await NestFactory.create(AppModule);
   await app.listen(port);
   console.log(`Your serve is running on port: ${port}`);
 }
